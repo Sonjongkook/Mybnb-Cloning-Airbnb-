99b1d950149c76b23a2b2b0bb996b0b00097dee5
Sonjongkook <63791870+Sonjongkook@users.noreply.github.com>
2020-08-17 17:02:31 +0900

    Views and admin update
    update of Views and other stuff

--- lists/admin.py
+++ lists/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from . import models 
+from . import models
+
 
 @admin.register(models.List)
 class ListAdmin(admin.ModelAdmin):
 
-    pass
+    list_display = ("name", "user", "count_rooms")
+    search_fields = ("name",)
+    filter_horizontal = ("rooms",)
--- rooms/models.py
+++ rooms/models.py
@@ -12,21 +12,25 @@ class AbstractItem(core_models.TimeStampedModel):
 
     class Meta:
         abstract = True
-    
+
     def __str__(self):
         return self.name
 
 
 class RoomType(AbstractItem):
     """ Roomtype model Definition"""
+
     class Meta:
         verbose_name = "Room Type"
-        ordering = ['created']
+        ordering = ["created"]
+
 
 class Amenity(AbstractItem):
     """ Amenity moeld Definition"""
+
     class Meta:
         verbose_name_plural = "Amenities"
+
 
 class Facility(AbstractItem):
     """ Facility model Definition"""
@@ -34,21 +38,23 @@ class Facility(AbstractItem):
     class Meta:
         verbose_name_plural = "Facilities"
 
+
 class HouseRule(AbstractItem):
     """HouseRule Model Definition"""
+
     class Meta:
         verbose_name = "House Rule"
 
 
 class Photo(core_models.TimeStampedModel):
     """Photo Model Definition"""
+
     caption = models.CharField(max_length=80)
-    file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    file = models.ImageField(upload_to="room_photos")
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
-
 
 
 class Room(core_models.TimeStampedModel):
@@ -69,14 +75,35 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     # foreign key= manythings to one(like parent and child)
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE) #if user is delethd then room is also deleted
-    # many to many field = manythings to manythins
-    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity, blank=True)
-    facilities = models.ManyToManyField(Facility, blank=True)
-    house_rules = models.ManyToManyField(HouseRule, blank=True)
 
+    # if user is delethd then room is also deleted
+    # many to many field = manythings to manythins
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.city = str.capitalize(self.city)
+        super().save(*args, **kwargs)
+
+    def total_rating(self):
+        all_reviews = self.reviews.all()
+        all_ratings = 0
+        if len(all_reviews) >0:
+            for review in all_reviews:
+                all_ratings += review.rating_average()
+            return round(all_ratings / len(all_reviews))
+        return 0
+        
+
+
 
